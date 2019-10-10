62d4df1f2335eba63f372bc1358979ec45ea8f95
Neil Ong <neil.ong31@gmail.com>
2019-10-10 00:35:47 -0400

    Added Event, Contact, Address Class

--- BTSWebApp/src/app/app.module.ts
+++ BTSWebApp/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventCreateComponent } from './event-create/event-create.component';
+import { EventReadComponent } from './event-read/event-read.component';
+import { EventUpdateComponent } from './event-update/event-update.component';
+import { EventDeleteComponent } from './event-delete/event-delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventCreateComponent,
+    EventReadComponent,
+    EventUpdateComponent,
+    EventDeleteComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ BTSWebApp/src/app/events.ts
@@ -0,0 +1,18 @@
+import { Address } from './address'
+import { Contact } from './contact';
+
+export class Events {
+    ev_name: String;
+    ev_address: Address;
+    ev_category: String[];
+    ev_description: String;
+    ev_company: String;
+    ev_contact: Contact;
+    ev_coordinator: String;
+    ev_date: Date;
+    ev_attendees: String[];
+    ev_photo: String;
+    ev_private: Boolean;
+    ev_invitedUsers: String[];
+}
+
