2a6ecbb3a4591af7284a5e28435ee97147cf094b
Varshan Nagarajan <vnagarajan1@myseneca.ca>
2019-10-11 14:40:33 -0400

    fixed routing errors

--- BTSWebApp/src/app/app-routing.module.ts
+++ BTSWebApp/src/app/app-routing.module.ts
@@ -1,27 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { EventCreateComponent } from './event-create/event-create.component'
-import { EventDeleteComponent } from './event-delete/event-delete.component';
-import { EventReadComponent } from './event-read/event-read.component';
-import { EventUpdateComponent } from './event-update/event-update.component';
+import { EventCreateComponent } from './event-create/event-create.component';
 import { InvalidRouteComponent } from './invalid-route/invalid-route.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { TokenViewComponent } from './token-view/token-view.component';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  { path: "home", component: HomeComponent},
+  { path: 'home', component: HomeComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full'},
-  { path: "eventCreate", component: EventCreateComponent},
-  { path: "eventDelete", component: EventDeleteComponent},
-  { path: "eventRead", component: EventReadComponent},
-  { path: "eventUpdate", component: EventUpdateComponent},
-  { path: "userCreate", component: UserCreateComponent},
-  { path: "invalidRoute", component: InvalidRouteComponent},
-  { path: "navbar", component: NavbarComponent},
-  { path: "login", component: LoginComponent},
-  { path: "tokenView", component: TokenViewComponent}
+  { path: 'eventCreate', component: EventCreateComponent},
+  { path: 'userCreate', component: UserCreateComponent},
+  { path: 'invalidRoute', component: InvalidRouteComponent},
+  { path: 'navbar', component: NavbarComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'tokenView', component: TokenViewComponent},
 ];
 
 @NgModule({
--- BTSWebApp/src/app/app.module.ts
+++ BTSWebApp/src/app/app.module.ts
@@ -5,9 +5,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventCreateComponent } from './event-create/event-create.component';
-import { EventReadComponent } from './event-read/event-read.component';
-import { EventUpdateComponent } from './event-update/event-update.component';
-import { EventDeleteComponent } from './event-delete/event-delete.component';
 import { UserCreateComponent } from './user-create/user-create.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
@@ -19,12 +16,9 @@ import { LoginComponent } from './login/login.component';
   declarations: [
     AppComponent,
     EventCreateComponent,
-    EventReadComponent,
-    EventUpdateComponent,
-    EventDeleteComponent,
     UserCreateComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
     InvalidRouteComponent,
     TokenViewComponent,
     LoginComponent
