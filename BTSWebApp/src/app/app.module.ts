e8dc98973a8e2b4e8b8ee3de8cb39dc28283e19b
Varshan Nagarajan <vnagarajan1@myseneca.ca>
2019-10-11 14:15:18 -0400

    added user create component

--- BTSWebApp/src/app/app.module.ts
+++ BTSWebApp/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { EventCreateComponent } from './event-create/event-create.component';
 import { EventReadComponent } from './event-read/event-read.component';
 import { EventUpdateComponent } from './event-update/event-update.component';
 import { EventDeleteComponent } from './event-delete/event-delete.component';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { EventDeleteComponent } from './event-delete/event-delete.component';
     EventCreateComponent,
     EventReadComponent,
     EventUpdateComponent,
-    EventDeleteComponent
+    EventDeleteComponent,
+    UserCreateComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
