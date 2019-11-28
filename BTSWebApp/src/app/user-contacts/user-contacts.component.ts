18abbef2bd8c2feaa2afebf4e1b9cc371452fd1c
Julian Boyko <julian.boyko@outlook.com>
2019-11-27 21:22:21 -0500

    added search - can be used as a template

--- BTSWebApp/src/app/user-contacts/user-contacts.component.ts
+++ BTSWebApp/src/app/user-contacts/user-contacts.component.ts
@@ -11,9 +11,9 @@ import { Router } from '@angular/router';
 })
 export class UserContactsComponent implements OnInit {
 
-  searchBy: Number;
+  searchBy: String;
   searchBar: String;
-
+  contacts = new Array();
   id: String;
   user: User;
   constructor(
@@ -50,9 +50,54 @@ export class UserContactsComponent implements OnInit {
     console.log(c);
   }
 
-  onSubmit(): void { // when the search button is clicked
+   getContact(contactUserName: String): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.m.reqresUserGetByUsername(contactUserName).subscribe(c => {
+        resolve(c[0]);
+      });
+    });
+  }
+
+  search(searchOption: String, searchValue: String): void {
+    switch(searchOption) {
+      case "name": {
+        console.log("name");
+        break;
+      }
+      case "company": {
+        console.log("company");
+        break;
+      }
+      case "event": {
+        console.log("event found");
+        break;
+      }
+      default: {
+        console.log("fail");
+        break;
+      }
+    }
+  }
+
+  onSubmit(): void{ // when the search button is clicked
     if(this.searchBy && this.searchBar) {
-      
+      if(this.contacts.length == 0) {
+        
+        for(var i = 0; i < this.user.user_contacts.length; i++) {
+           this.getContact(this.user.user_contacts[i]).then((contact) => {
+            this.contacts.push(contact);
+            console.log("Contact: " + contact);
+          }).catch((err) => {
+            console.log(err);
+          });
+        }
+        this.search(this.searchBy, this.searchBar);
+        
+      } else {
+        // contact array is already prevented. this allows a second search to be faster and not refetch all data again
+        this.search(this.searchBy, this.searchBar);
+      }
+     
     } else {
       console.log("null searchBy or searchBar");
     }
