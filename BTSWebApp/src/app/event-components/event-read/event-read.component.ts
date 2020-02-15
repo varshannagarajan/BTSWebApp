8a18b11be66a5411b33e9de9521fc7331466e2a3
Julian Boyko <julian.boyko@outlook.com>
2020-02-15 17:02:10 -0500

    added google maps api

--- BTSWebApp/src/app/event-components/event-read/event-read.component.ts
+++ BTSWebApp/src/app/event-components/event-read/event-read.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { EventService } from '../../services/event.service';
 import { Events } from '../../classes/events';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/classes/user';
 import { UserService } from 'src/app/services/user.service';
+import { MapMarker, MapInfoWindow } from '@angular/google-maps'
 
 @Component({
   selector: 'app-event-read',
   templateUrl: './event-read.component.html'
 })
 export class EventReadComponent implements OnInit {
+  @ViewChild(MapInfoWindow, { static: false }) info: MapInfoWindow
+
   id: String;
   event: Events;
   attendeeUsers: User[];
+
+  // Google Maps
+  center: google.maps.LatLngLiteral;
+  geocoder = new google.maps.Geocoder();
+  markers = [];
+  infoContent = '';
 
   constructor(
     private route: ActivatedRoute,
@@ -26,6 +35,9 @@ export class EventReadComponent implements OnInit {
     const id = this.route.snapshot.params['_id'];
     this.e.eventGetByCode(id).subscribe(s => {
       this.event = s;
+
+      this.setEventLocationToGoogleMaps()
+
       this.e.eventSet(this.event);
       for (let i = 0; i < this.event.ev_attendees.length; i++) {
         this.u
@@ -33,7 +45,48 @@ export class EventReadComponent implements OnInit {
           .subscribe(s => {
             this.attendeeUsers.push(s);
           });
+        }
+    });
+  }
+
+  setEventLocationToGoogleMaps() {
+    var address = this.event.ev_address.street + ", " + this.event.ev_address.city + ", " + this.event.ev_address.province;
+    console.log(address);
+    this.geocoder.geocode({'address': address}, (results, status) => {
+      if(status == "OK") {
+        this.center = {
+          lat: results[0].geometry.location.lat(),
+          lng: results[0].geometry.location.lng(),
+        }
+        this.addMarker(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+      } else {
+        console.log("geocoder error");
       }
     });
   }
+
+  addMarker(passedInLat, passedInLong) {
+    this.markers.push({
+      position: {
+        lat: passedInLat,
+        lng: passedInLong,
+      },
+      label: {
+        color: 'red',
+        text: this.event.ev_name,
+      },
+      title: this.event.ev_name,
+      info: this.event.ev_description,
+      options: {
+        animation: google.maps.Animation.BOUNCE,
+      },
+    })
+
+  }
+
+  openMarkerInfo(marker: MapMarker, content) {
+    this.infoContent = content
+    this.info.open(marker)
+  }
+
 }
