aa5dd79f7481c14397bd627e1b459a2f1c9cdd2f
Varshan Nagarajan <vnagarajan1@myseneca.ca>
2020-03-23 17:05:23 -0400

    Fix #45 Update Event Feed

--- BTSWebApp/src/app/classes/address.ts
+++ BTSWebApp/src/app/classes/address.ts
@@ -4,5 +4,4 @@ export class Address {
     city: String;
     province: String;
     country: String;
-    
 }
--- BTSWebApp/src/app/event-components/event-feed/event-feed.component.ts
+++ BTSWebApp/src/app/event-components/event-feed/event-feed.component.ts
@@ -4,6 +4,8 @@ import { UserService } from '../../services/user.service';
 import { User } from '../../classes/user';
 import { EventService } from '../../services/event.service';
 import { Events } from '../../classes/events';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Address } from 'src/app/classes/address';
 
 @Component({
   selector: 'app-event-feed',
@@ -12,13 +14,16 @@ import { Events } from '../../classes/events';
 export class EventFeedComponent implements OnInit {
   user: User;
   userEvents: Events[];
+  term: string;
   constructor(
     private route: ActivatedRoute,
     private m: UserService,
     private e: EventService,
-    private router: Router
+    private router: Router,
+    private sanitizer: DomSanitizer
   ) {
     this.userEvents = new Array();
+    this.term = '';
   }
 
   ngOnInit() {
@@ -37,5 +42,22 @@ export class EventFeedComponent implements OnInit {
 
   viewEvent(e: string) {
     this.router.navigate(['/eventRoom/', e]);
+  }
+
+  mapsLink(address: Address) {
+    return this.sanitizer.bypassSecurityTrustUrl(
+      'https://www.google.com/maps/search/?api=1&query=' +
+        encodeURIComponent(
+          address.street +
+            ' ' +
+            address.city +
+            ' ' +
+            address.province +
+            ' ' +
+            address.postalCode +
+            ' ' +
+            address.country
+        )
+    );
   }
 }
