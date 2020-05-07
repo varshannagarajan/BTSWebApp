03e72b57d868dc3c3468c53823c23f1a42770c99
Neil <neil.ong31@gmail.com>
2020-05-07 13:06:02 -0400

    Sizing and spacing done

--- BTSWebApp/src/app/navbar/navbar.component.ts
+++ BTSWebApp/src/app/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: []
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
   constructor(public auth: AuthService) {}
